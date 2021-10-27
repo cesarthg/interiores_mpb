a98f4eeab27fbeeaa9bb790e6378f2da03fc0ada
César H.G. | @cesarthg <cesar.hg@me.com>
2021-10-27 16:46:29 -0500

    Se anexa la introduccion alos estilos de baño

---
+++ intro.php
@@ -0,0 +1,56 @@
+<?php include './header.php'; ?>
+<section>
+        <div class="row no-gutters">
+            <div class="col-lg-2 border-right">
+                <div class="d-flex h-100"></div>
+            </div>
+            <div class="col-lg-9 mt-4 mx-3">
+                <h1 class="h1-txt">La renovación de tus sueños, inspiración para un espacio con personalidad.</h1>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <!-- Menu lateral -->
+            <div class="col-lg-2 border-right"></div>
+            <!-- Texto principal -->
+            <div class="col-lg-5 mt-4 mx-3">
+                <p class="article-txt">Pasarás mucho tiempo en el baño, desde la primera hora de la mañana hasta el último momento antes de dormir. Es necesario tomar el tiempo para pensar cómo quiero <strong>MI ESPACIO</strong>, y asi elegir el estilo que quieres darle a tu baño y que perosnalidad tendrá.</p>
+                <p class="article-txt">La planificación es lo más importate- todo requiere tiempo y dinero. Y planer un lugar que funcione para todo momento puede ser complicado, para no generar gastos adicionales será necessario una pequeña lista de tareas y necesidades básicas en el uso. Así cubrimos las necesidades de espacio. Si es un baño compelto por ejemplo, necesitamos espacio de sólo de ducha o requieres de una tina de baño porque tienes niños, necesitas almacenamientos indivudual para una persona o quiza dos? - requieres un tocador para maquillaje o peinado? quieres un espacio de barbería? Cada acción que hagas es necesaria pensarla y analizar si quieres detalles en especifico.</p>
+                <p class="article-txt">Los elementos del diseño interior son : formas, texturas colores e iluminación, y estos deben de estar armonía para que todo sea perfecto, cómo será que la luz juega con la superficie de algunas baldosas, acentuando las características de los accesorios elejidos. Con tantos estilos para elegir, puede ser un desafío no terminar con una mezcla que no tenga coherencia. Claro que puedes jugar y arriesgarte, pero también requieres de un balance y equilibrio.</p>
+                <p class="article-txt">La estética , es cómo se ve y se siente y será lo más importante para estar feliz en un espacio y sentirte cómodo. Busca las cosas que actulamente te hacen falta y convierte tu nuevo espacio en eso que ahora mismo no tienes, adáptalo a tus gustos, tu hogar. <strong>A TU MEDIDA</strong></p>
+            </div>
+            <!-- Foto lateral derecha -->
+            <div class="col-lg-4 mt-4">
+                <img class="img-intro img-fluid" src="./img/introduccion.jpg" alt="">
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-lg-2 border-right"></div>
+            <div class="col-lg-3 mx-4 mt-5 plan-card">
+                <div class="mx-4 my-4">
+                    <h2 class="article-txt h4 text-white h4-esp"><strong>PLANNING IS THE KEY</strong></h2>
+                    <p class="article-txt">La renovación de tu baño, empieza desde el momento en que empeizas a pensar en un cambio, y empeizas a idealizar nuevas formas, texturas, muebles, decoración. En tu cabeza, la idea empezó a tomar forma.</p>
+                    <h2 class="article-txt h4 "><strong>ESO ES EL INICIO.</strong></h2>
+                    <p class="article-txt">Donde empezar, es lo más complejo, tienes que pensar en descubrir que estilo te gusta y empezar a buscar inspiración.</p>
+                    <p class="article-txt">Nosotros te daremos las bases para el proyecto de renovación que estabas buscando. <br> INSPIRACIÓN - IDEAS - PROYECTO.</p>
+                </div>
+            </div>
+            <div class="col-lg-5 mx-4 mt-5">
+                <h2 class=" h2-esp">El poder de la planeación</h2>
+                <p class="article-txt">¿Cuál es tu presupuesto?</p>
+                <p class="article-txt">Saber cuanto es lo que quieres gastar es probablemnte lo menos divertido del poryecto, sin embargo es necesario para la renovación. De acuerdo a esta desición es como empezremos a tomar desiciones. Podrás empezar a elegir losetas, pinturas, colores, pero lo más importante cuanto vas a invertir en muebles y accesorios para darle el estilo ideal a tu baño y depués los detalles finales con la decoraciñon según el estilo elegido.</p>
+                <p class="article-txt">Tenemos la posibilidad de ofrecerte muebles y accesorios para todos los presupuestos.Te daremos la shoping list completa para cubrir las necesidades de tu bano, además de sugerencias en los detalles de decoración.</p>
+                <h2 class="h4 h2-esp">Planear el layout de tu baño</h2>
+                <p class="article-txt">¿Donde irá todo? ¿Lo que tienes esta organizado o requieres una nueva disposición y acomodo?</p>
+                <p class="article-txt">No te encierres en la estructura preestablecida que tienes actualmente, para eso son las renovaciones, son una perfecta oportunidad para hacer nuevos planes tu baño será lo que tu quieras! Sólo recuerda que si quieres cambiar todo, deberás hacer reformas más costosas y que llevaran más tiempo. TODO DEPENDERA DEL PRESUPUESTO.</p>
+            </div>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
